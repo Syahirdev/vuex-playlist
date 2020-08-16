8712cc6e32630dd20b3738991d5856f55aa1c040
Syahir <888syahir@gmail.com>
2020-08-17 00:24:30 +0800

    adding ...mapGetters & ...mapActions for multiple getters & actions
    simple. use babel & es6 ...map functions.

--- vuex-playlist/src/components/ProductListOne.vue
+++ vuex-playlist/src/components/ProductListOne.vue
@@ -12,19 +12,24 @@
 </template>
 
 <script>
+import { mapActions, mapGetters } from 'vuex';
 export default {
 	computed: {
 		products() {
 			return this.$store.state.products;
 		},
-		saleProducts() {
-			return this.$store.getters.saleProducts;
-		},
+		// saleProducts() {
+		// 	return this.$store.getters.saleProducts;
+		// },
+
+		...mapGetters(['saleProducts']),
 	},
 	methods: {
-		reducePrice(amount) {
-			this.$store.dispatch('reducePrice', amount);
-		},
+		// reducePrice(amount) {
+		// 	this.$store.dispatch('reducePrice', amount);
+		// },
+
+		...mapActions(['reducePrice']),
 	},
 };
 </script>
